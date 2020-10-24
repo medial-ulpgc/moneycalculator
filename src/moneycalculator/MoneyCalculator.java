baf00497d929b5c6c1111d3aae3c6132d22ee2d0
alejandro Medina <alejandro.medina104@alu.ulpgc.es>
2020-10-24 01:05:30 +0100

    version2 (IPO)

--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -2,17 +2,35 @@ package moneycalculator;
 import java.util.Scanner;
 
 public class MoneyCalculator {
-
+    private Double amount = 0.;
+    private Double convertedAmount=0.;
+    
     public static void main(String[] args)  {
-        System.out.println("Introduce una cantidad en dólares: ");
-        Scanner scanner = new Scanner(System.in);
-        double amount = Double.parseDouble(scanner.next());
-        double exchangeRate = getExchangeRate("USD","EUR");
-        System.out.println("" + exchangeRate * amount );
+        new MoneyCalculator().execute();
     }
     private static double getExchangeRate(String from, String to) {
                 return 0.86;
-        
+     
+    }
+    private void execute(){
+        input();
+        process();
+        output();
+    }
+
+    private void input() {
+        System.out.println("Introduce una cantidad en dólares: ");
+        Scanner scanner = new Scanner(System.in);
+        amount = Double.parseDouble(scanner.next());
+    }
+
+    private void output() {
+        System.out.println( amount+ " dólares se corresponden a "+ convertedAmount+ " euros.");
+    }
+
+    private void process() {
+        double exchangeRate = getExchangeRate("USD","EUR");
+        convertedAmount = exchangeRate*amount;
     }
     
 }
